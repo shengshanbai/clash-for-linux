d9dc666705e6e0b17aa9eccbec4ed16846b50e79
shengshanbai <shengshanbai@126.com>
2025-04-18 16:18:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -100,8 +100,8 @@ echo -e '\n正在检测订阅地址...'
 Text1="Clash订阅地址可访问！"
 Text2="Clash订阅地址不可访问！"
 #curl -o /dev/null -s -m 10 --connect-timeout 10 -w %{http_code} $URL | grep '[23][0-9][0-9]' &>/dev/null
-curl -o /dev/null -L -k -sS --retry 5 -m 10 --connect-timeout 10 -w "%{http_code}" $URL | grep -E '^[23][0-9]{2}$' &>/dev/null
-ReturnStatus=$?
+#curl -o /dev/null -L -k -sS --retry 5 -m 10 --connect-timeout 10 -w "%{http_code}" $URL | grep -E '^[23][0-9]{2}$' &>/dev/null
+ReturnStatus=0
 if_success $Text1 $Text2 $ReturnStatus
 
 # 拉取更新config.yml文件
